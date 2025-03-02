7c4c74e2fbc5a31390c831b88b638d033719c0e2
AlexandraFefler <sasha.fefler@gmail.com>
2025-03-02 07:07:33 +0200

    o

--- ec2_provision/backend.tf
+++ ec2_provision/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "spacecapy_bucket"
+    bucket         = "spacecapy-bucket"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
     encrypt        = true
