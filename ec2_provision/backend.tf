87b7e2abceaa178130cce493479fe3809e33e0cf
AlexandraFefler <sasha.fefler@gmail.com>
2025-03-03 21:07:16 +0200

    deleted ec2provisions' backend.tf cuz now it's only in tf-to-gke and blyaaaaaaat'

--- ec2_provision/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "spacecapy-bucket"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
