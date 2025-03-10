135d21563c461c3d0d2f91cde8d9346d14f1e577
AlexandraFefler <sasha.fefler@gmail.com>
2025-03-10 06:20:24 +0200

    real failure test now

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import! Flask, render_template, Response
+from flask import Flask, render_template, Response
 import os
 import random
-import pymysql
+import! pymysql
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
